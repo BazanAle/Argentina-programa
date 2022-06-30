316f8645a8070425261b50823871c5d776d39888
Alejandro Bazan <alejandrobazan@MacBook-Pro-de-Alejandro.local>
2022-06-30 13:17:06 -0300

    prueba

---
+++ PROYECTO FINAL/BackEnd/Springboot/src/main/java/com/miportfolio/Springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.miportfolio.Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
---
+++ PROYECTO FINAL/BackEnd/Springboot/src/test/java/com/miportfolio/Springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miportfolio.Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
